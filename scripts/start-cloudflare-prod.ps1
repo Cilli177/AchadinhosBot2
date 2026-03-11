98ab229ef6b08851998556b638f7c16cfad2bc89
Thiago Cilli <cilli177@gmail.com>
2026-03-11 02:40:33 -0300

    chore: sanitize tracked environment templates

--- scripts/start-cloudflare-prod.ps1
+++ scripts/start-cloudflare-prod.ps1
@@ -1,14 +1,22 @@
 param(
-    [string]$AppUrl = "http://127.0.0.1:5005"
+    [string]$AppUrl = "http://127.0.0.1:5005",
+    [string]$TunnelName = "achadinhos-fixed",
+    [string]$Hostname = "achadinhos.reidasofertas.ia.br",
+    [string]$BioHostname = "bio.reidasofertas.ia.br"
 )
 
 Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 
 & (Join-Path $PSScriptRoot "start-cloudflare-tunnel.ps1") `
-    -TunnelName "achadinhos-fixed" `
-    -Hostname "achadinhos.reidasofertas.ia.br" `
+    -TunnelName $TunnelName `
+    -Hostname $Hostname `
     -AppUrl $AppUrl `
     -ConfigFileName "config.prod.yml" `
     -LogPrefix "prod" `
     -ConfigUpdateScope "Production"
+
+if (-not [string]::IsNullOrWhiteSpace($BioHostname)) {
+    $cloudflared = (Get-Command "cloudflared" -ErrorAction Stop).Source
+    & $cloudflared tunnel route dns $TunnelName $BioHostname 2>$null
+}
